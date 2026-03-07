62282fab3458e827df5d750628f6703010d24653
rk868726-alt <rk868726@gmail.com>
2026-03-07 19:25:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -462,6 +462,45 @@ if (user.xp >= xpNeeded) {
 
 saveLevels(levels);
 
+  //play
+
+  module.exports = {
+  data: {
+    name: "play",
+    description: "Play music"
+  },
+
+  async execute(interaction) {
+
+    const query = interaction.options.getString("song");
+    const vc = interaction.member.voice.channel;
+
+    if (!vc)
+      return interaction.reply("Join a voice channel first.");
+
+    const player = manager.create({
+      guild: interaction.guild.id,
+      voiceChannel: vc.id,
+      textChannel: interaction.channel.id
+    });
+
+    player.connect();
+
+    const res = await manager.search(query, interaction.user);
+
+    if (!res.tracks.length)
+      return interaction.reply("No results found.");
+
+    player.queue.add(res.tracks[0]);
+
+    if (!player.playing)
+      player.play();
+
+    interaction.reply(`🎵 Playing **${res.tracks[0].title}**`);
+  }
+};
+
+
  
 
   // ===== MIRROR MESSAGE =====
@@ -1393,6 +1432,7 @@ cron.schedule("*/5 * * * *", async () => {
   }
 
 });
+
 
 
 
