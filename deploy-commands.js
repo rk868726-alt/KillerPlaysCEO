b95bf15ed59cb098657a868bb4c3ad8e4a6948f7
rk868726-alt <rk868726@gmail.com>
2026-03-09 13:40:43 +0530

    Add files via upload

--- deploy-commands.js
+++ deploy-commands.js
@@ -2,7 +2,7 @@ const { REST, Routes } = require('discord.js');
 const fs = require('fs');
 
 const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('./commands');
 
 for (const file of commandFiles) {
   const command = require(`./commands/${file}`);
@@ -11,19 +11,7 @@ for (const file of commandFiles) {
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
 
-(async () => {
-  try {
-
-    console.log("Refreshing slash commands...");
-
-    await rest.put(
-      Routes.applicationCommands(process.env.CLIENT_ID),
-      { body: commands },
-    );
-
-    console.log("Slash commands deployed.");
-
-  } catch (error) {
-    console.error(error);
-  }
-})();
+rest.put(
+  Routes.applicationCommands('YOUR_CLIENT_ID'),
+  { body: commands },
+);
